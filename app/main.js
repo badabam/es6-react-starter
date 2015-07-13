d0351602688a3f473475127e69a947f6f869ac59
Will Faurot <wfaurot@gmail.com>
2015-07-13 02:28:55 -0600

    Move top-level component out of main.js

--- app/main.js
+++ app/main.js
@@ -1,11 +1,12 @@
+/**
+ * main.js is webpack's entry point, meaning here you'll typically
+ * require your most top-level React componnent and render it with
+ * `React.render`.
+ */
 import React from 'react';
+import App from './App';
 
+// Use webpack's style loader to inject css.
 require('./styles/app.css');
-
-class App extends React.Component {
-  render() {
-    return <div>Hi mom!</div>
-  }
-}
 
 React.render(<App />, document.getElementById('root'));
