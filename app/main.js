00402b6020f284fcc9399bd87ea6d8b08ac8f533
Will Faurot <wfaurot@gmail.com>
2015-08-30 16:39:10 -0700

    Clean up

--- app/main.js
+++ app/main.js
@@ -1,12 +1,6 @@
-/**
- * main.js is webpack's entry point, meaning here you'll typically
- * require your most top-level React componnent and render it with
- * `React.render`.
- */
 import React from 'react';
 import App from './App';
 
-// Use webpack's style loader to inject css.
 require('./styles/app.css');
 
 React.render(<App />, document.getElementById('root'));
